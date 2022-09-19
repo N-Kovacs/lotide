b5febfb21a577f0c0208c1f99e057ca17ebc00f9
Nicholas Kovacs <nicholasderekkovacs@gmail.com>
2022-09-19 18:03:07 +0000

    head function

---
+++ head.js
@@ -0,0 +1,20 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+const head = function(inputArray) {
+  if (inputArray.length > 0) {
+    return inputArray[0];
+  } else {
+    return undefined;
+  }
+};
+
+// TEST CODE
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
