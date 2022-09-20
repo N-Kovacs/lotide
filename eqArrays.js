41bb52829b062d95bd7b9da32869b36be3067058
Nicholas Kovacs <nicholasderekkovacs@gmail.com>
2022-09-20 19:50:07 +0000

    implemented eqArrays

---
+++ eqArrays.js
@@ -0,0 +1,26 @@
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(firstArray, secondArray) {
+  for (let i = 0; i < firstArray.length; i++) {
+    if (firstArray[i] !== secondArray[i]){
+      return false
+    }
+  }
+  return true
+};
+
+if(1 != "1"){
+  console.log("but why")
+}
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true) // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false) // => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true) // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false) // => false
