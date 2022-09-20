ffb04b8b09e2b45ccd5a8cdb38ac31f0d3493a60
Nicholas Kovacs <nicholasderekkovacs@gmail.com>
2022-09-20 21:10:37 +0000

    fixed without to actually work with multiple removals

--- without.js
+++ without.js
@@ -10,12 +10,16 @@ const assertArraysEqual = function(firstArray, secondArray) {
 
 const without = function(inputArray, removedArray) {
   let outputArray = inputArray.slice(0);
+  let matchs = [];
   for (let i = 0; i < inputArray.length; i++) {
     for (let j = 0; j < removedArray.length; j++) {
       if (inputArray[i] === removedArray[j]) {
-        outputArray.splice(i, 1);
+        matchs.push(i);
       }
     }
+  }
+  for (let i = (matchs.length - 1); i >= 0; i--) {
+    outputArray.splice(matchs[i], 1);
   }
   return outputArray;
 };
@@ -24,8 +28,10 @@ assertArraysEqual(without([1, 2, 3], [1]), [2, 3]);
 assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
 
 assertArraysEqual(without(["1", "2", "3", "4"], [1, 2, "3"]), ["1", "2", "4"]);
+assertArraysEqual(without([2, 1, 2, 3], [1, 2]), [3]);
+assertArraysEqual(without([3, 2, 1, 2, 3], [1, 2]), [3,3]);
 
 const words = ["hello", "world", "lighthouse"];
 without(words, ["lighthouse"]); // no need to capture return value for this test case
-// Make sure the original array was not altered by the without function
+//Make sure the original array was not altered by the without function
 assertArraysEqual(words, ["hello", "world", "lighthouse"]);
