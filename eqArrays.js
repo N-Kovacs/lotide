b515dc9997d072fa7e0dfab9f1d1b6d35066d647
Nicholas Kovacs <nicholasderekkovacs@gmail.com>
2022-09-20 19:54:22 +0000

    lint eqarrays

--- eqArrays.js
+++ eqArrays.js
@@ -9,18 +9,15 @@ const assertEqual = function(actual, expected) {
 
 const eqArrays = function(firstArray, secondArray) {
   for (let i = 0; i < firstArray.length; i++) {
-    if (firstArray[i] !== secondArray[i]){
-      return false
+    if (firstArray[i] !== secondArray[i]) {
+      return false;
     }
   }
-  return true
+  return true;
 };
 
-if(1 != "1"){
-  console.log("but why")
-}
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true) // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false) // => false
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);// => false
 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true) // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false) // => false
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
