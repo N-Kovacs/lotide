928952b76b0b8d0541f91dfbc5aa2f6ad6b5801b
Nicholas Kovacs <nicholasderekkovacs@gmail.com>
2022-09-21 20:11:11 +0000

    eslint and assert object equal implementation

--- assertObjectEqual.js
+++ assertObjectEqual.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(firstArray, secondArray) {
   
   let arrayMaxLength = firstArray.length;
@@ -49,17 +41,13 @@ const eqObjects = function(object1, object2) {
   return true;
 };
 
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  if (eqObjects(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed, ${inspect(actual)} is equal to ${inspect(expected)}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed, ${inspect(actual)} is not equal to ${inspect(expected)}`);
+  }
+};
 
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-assertEqual(eqObjects(ab, ba), true); // => true
-
-const abc = { a: "1", b: "2", c: "3" };
-assertEqual(eqObjects(ab, abc), false); // => false
-
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-assertEqual(eqObjects(cd, dc), true); // => true
-
-const cd2 = { c: "1", d: ["2", 3, 4] };
-assertEqual(eqObjects(cd, cd2), false); // => false
+assertObjectsEqual({a:2, b:4}, {a:2,b:3});
